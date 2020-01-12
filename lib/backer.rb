d7d6d82290796e4f0298686ad3a03d54323a8a95
Chris Morgan <cwm111794@gmail.com>
2020-01-12 12:39:45 -0600

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,15 @@
+class Backer
+    attr_reader :name
+    attr_accessor :backed_projects
+    def initialize(name)
+        @name = name
+        @backed_projects = []
+    end
+
+    def back_project(title)
+        @backed_projects << title
+        title.add_backer(self) unless title.backers.include?(self)
+
+    end
+
+end
