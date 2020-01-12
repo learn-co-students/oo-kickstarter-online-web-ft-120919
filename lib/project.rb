16173887db89691303eb1398d15cdfc07598699e
Chris Morgan <cwm111794@gmail.com>
2020-01-12 12:41:27 -0600

    Done.

--- lib/project.rb
+++ lib/project.rb
@@ -1,14 +1,15 @@
+# frozen_string_literal: true
+
 class Project
-    attr_reader :title
-    attr_accessor :backers
-    def initialize(title)
-        @title = title
-        @backers = []
-    end
+  attr_reader :title
+  attr_accessor :backers
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
 
-    def add_backer(name)
-        @backers << name
-        name.back_project(self) unless name.backed_projects.include?(self)
-
-    end
+  def add_backer(name)
+    @backers << name
+    name.back_project(self) unless name.backed_projects.include?(self)
+  end
 end
