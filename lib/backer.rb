f4b16a78ad94db76340b7312daac37c79987628b
Chris Morgan <cwm111794@gmail.com>
2020-01-12 12:44:11 -0600

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,15 +1,16 @@
+# frozen_string_literal: true
+
+# classs backer is inititalized with name and empty array
 class Backer
-    attr_reader :name
-    attr_accessor :backed_projects
-    def initialize(name)
-        @name = name
-        @backed_projects = []
-    end
+  attr_reader :name
+  attr_accessor :backed_projects
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
 
-    def back_project(title)
-        @backed_projects << title
-        title.add_backer(self) unless title.backers.include?(self)
-
-    end
-
+  def back_project(title)
+    @backed_projects << title
+    title.add_backer(self) unless title.backers.include?(self)
+  end
 end
--- lib/project.rb
+++ lib/project.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# class project is initialized with title and an empty array
 class Project
   attr_reader :title
   attr_accessor :backers
@@ -7,7 +8,7 @@ class Project
     @title = title
     @backers = []
   end
-
+  # should add a backer to array
   def add_backer(name)
     @backers << name
     name.back_project(self) unless name.backed_projects.include?(self)
